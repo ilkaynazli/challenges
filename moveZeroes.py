29ead307c442dc2266552a99472ea1540158d24c
ilkaynazli <ilkayncelik@gmail.com>
2018-12-08 23:08:36 -0800

    commonWord & moveZeroes

---
+++ moveZeroes.py
@@ -0,0 +1,29 @@
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+"""
+#this solution can be faster!
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        
+        j = 0
+        while j < len(nums) - 1:
+            i = 0
+            while i < len(nums)-1:
+                if nums[i] == 0:
+                    nums[i], nums[i+1] = nums[i+1], nums[i]
+                i += 1
+
+            j += 1
