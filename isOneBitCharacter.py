d7b3e027fdd3d59d6214083340bdbed47e4748b4
ilkaynazli <ilkayncelik@gmail.com>
2018-12-26 22:23:40 -0800

    challenges

---
+++ isOneBitCharacter.py
@@ -0,0 +1,42 @@
+"""
+We have two special characters. The first character can be represented by one bit 0. The second character can be represented by two bits (10 or 11).
+
+Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
+
+Example 1:
+Input: 
+bits = [1, 0, 0]
+Output: True
+Explanation: 
+The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+Example 2:
+Input: 
+bits = [1, 1, 1, 0]
+Output: False
+Explanation: 
+The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
+Note:
+
+1 <= len(bits) <= 1000.
+bits[i] is always 0 or 1.
+"""
+
+class Solution:
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        if len(bits) == 1:
+            return True
+        i = 0
+        order = []
+        while i <= len(bits) - 1:
+            if bits[i] == 1 and (bits[i+1] == 1 or bits[i+1] == 0):
+                i += 2
+                order.append(False)
+            elif bits[i] == 0:
+                i += 1
+                order.append(True)
+        
+        return order[-1]
