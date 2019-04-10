98e38279aad110c7e500233ffdbee7c65fd8f1a9
ilkaynazli <ilkayncelik@gmail.com>
2019-04-09 18:41:17 -0700

    challenges

--- findAnagrams.py
+++ findAnagrams.py
@@ -30,36 +30,35 @@ The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
 """
 
-class Solution:
+class Solution:        
     def findAnagrams(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: List[int]
         """
-
-        p_dict = {}
-        for char in p:
-            if char in p_dict:
-                p_dict[char] += 1
-            else:
-                p_dict[char] = 1
+        n = len(p)
+        m = len(s)
+        if n > m:
+            return []
         
         result = []
-        i = 0
-        while i <= len(s) - len(p):
-            temp = {}
-            j = i
-            while j < len(p) + i:
-                if s[j] in temp:
-                    temp[s[j]] += 1
-                else:
-                    temp[s[j]] = 1
-                j += 1
-            if temp == p_dict:
+        c_p = collections.Counter(p)
+        c_s = collections.Counter(s[:n])
+        i = 0 
+        j = n
+        while j < m:
+            # print(c_s, c_p, i, j)
+            if c_s == c_p:
                 result.append(i)
+            c_s[s[i]] -= 1
+            if c_s[s[i]] == 0:
+                del c_s[s[i]]
+            c_s[s[j]] += 1
             i += 1
-            
+            j += 1
+        if c_s == c_p:
+            result.append(i)
         return result
 
 """
